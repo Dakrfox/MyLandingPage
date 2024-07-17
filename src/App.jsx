0c05ce39faa2081c67be8037bee18febf6e41417
jormundgandr <business.nimh@outlook.com>
2024-07-17 13:50:39 -0500

    fix href services

--- src/App.jsx
+++ src/App.jsx
@@ -270,7 +270,7 @@ function App() {
         <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4  lg:pl-20 lg:pr-20 md:pl-10 md:pr-10 pb-20">
           <div className="bg-gray-100 p-4">
             <a
-              href="#"
+              
               className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
             >
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -285,7 +285,7 @@ function App() {
           </div>
           <div className="bg-gray-100 p-4">
             <a
-              href="#"
+              
               className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
             >
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -299,7 +299,7 @@ function App() {
           </div>
           <div className="bg-gray-100 p-4">
             <a
-              href="#"
+              
               className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
             >
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -314,7 +314,7 @@ function App() {
           </div>
           <div className="bg-gray-100 p-4">
             <a
-              href="#"
+              
               className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
             >
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
